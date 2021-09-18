50c705421ab9ff813713752272b42160a08470c4
farid555 <mohammad.uzzman2@centria.fi>
2021-09-19 02:15:17 +0300

    added Announcement

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from "../components/Nabar"
+import Announcement from "../components/Announcement"
 
 const Home = () => {
     return (
         <div>
+            <Announcement />
             <Navbar />
+
         </div>
     )
 }
